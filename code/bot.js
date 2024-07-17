a6e32cc7ddcb267e35a9e68cb5230e34fffdeeeb
porter <porterbmoody@gmail.com>
2024-07-16 22:56:36 -0600

    f

--- code/bot.js
+++ code/bot.js
@@ -59,11 +59,11 @@ class Bot {
     async scrapeGroupProfileURLs(group_url) {
         await this.page.goto(group_url, { waitUntil: 'networkidle2' });
         await this.page.waitForSelector("[class='x1i10hfl xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz x1sur9pj xkrqix3 xzsf02u x1s688f']", { timeout: 10000 });
-        // await this.autoScroll();
-        // await this.sleep(2000);
-        // await this.autoScroll();
-        // await this.sleep(2000);
-        // await this.autoScroll();
+        await this.autoScroll();
+        await this.sleep(2000);
+        await this.autoScroll();
+        await this.sleep(2000);
+        await this.autoScroll();
 
         const groupProfileURLs = await this.page.evaluate((selector) => {
             const elements = document.querySelectorAll(selector);
@@ -80,10 +80,10 @@ class Bot {
         return groupProfileURLs;
     }
 
-    async scrapeProfileData(number_of_hours, group_url) {
+    async scrapeProfileData(time_between, group_url) {
         const groupProfileURLs = await this.scrapeGroupProfileURLs(group_url);
         const totalProfiles = groupProfileURLs.length;
-        const totalSecondsInDay = number_of_hours * 60 * 60;
+        const totalSecondsInDay = time_between * 60 * 60;
         const delayBetweenProfiles = totalSecondsInDay / totalProfiles * 1000;
         console.log("Unique profile URLs to scrape:");
         console.log(groupProfileURLs.length);
@@ -103,7 +103,7 @@ class Bot {
                 await this.page.waitForSelector("[class='x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x14qwyeo xw06pyt x579bpy xjkpybl x1xlr1w8 xzsf02u x2b8uid']", { timeout: 5000 });
     
                 const profile_name = await this.page.$eval("[class='x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x14qwyeo xw06pyt x579bpy xjkpybl x1xlr1w8 xzsf02u x2b8uid']", element => element.textContent.trim());
-
+                console.log(`scraping...${profile_name}`);
                 await this.page.waitForSelector("[class='x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x3x7a5m x1f6kntn xvq8zen xo1l8bm xzsf02u x1yc453h']", { timeout: 5000 });
     
                 const marital_status = await this.page.evaluate((selector) => {
@@ -136,7 +136,7 @@ class Bot {
         }
     }
 
-    async runBot(username, password, group_url, number_of_hours) {
+    async runBot(username, password, group_url, time_between) {
         try {
             await new Promise(resolve => setTimeout(resolve, 2000));
 
@@ -151,7 +151,7 @@ class Bot {
             this.page = await this.browser.newPage();
             await this.login(username, password);
 
-            await this.scrapeProfileData(number_of_hours, group_url);
+            await this.scrapeProfileData(time_between, group_url);
 
         } catch (error) {
             console.error('An error occurred:', error);
@@ -169,13 +169,13 @@ app.get('/', (req, res) => {
 });
 
 app.post('/run-bot', async (req, res) => {
-    const { username, password, group_url, number_of_hours } = req.body;
+    const { username, password, group_url, time_between } = req.body;
 
-    console.log('Received request to run bot with:', { username, password, group_url, number_of_hours });
+    console.log('Received request to run bot with:', { username, password, group_url, time_between });
 
     try {
         const bot = new Bot();
-        await bot.runBot(username, password, group_url, number_of_hours);
+        await bot.runBot(username, password, group_url, time_between);
         res.send('Bot execution completed successfully!');
     } catch (error) {
         console.error('Error running bot:', error);
