02106cb641bf41b94708a73bbb08dd4df804020e
porter <porterbmoody@gmail.com>
2024-07-20 11:26:17 -0600

    'f'

--- package.bat
+++ package.bat
@@ -3,7 +3,10 @@ echo Packaging the bot...
 
 pkg . --targets node18-win-x64 --output bot.exe --public
 
-
+echo Pushing to GitHub...
+git add .
+git commit -m "Packaged bot update"
+git push
 
 echo Done!
 pause
